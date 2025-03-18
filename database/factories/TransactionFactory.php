5fe5e14a52bcd57f27197604bc13e09271f6c908
M'hamed Ouhanane <mhmdeouhnane60@gmail.com>
2025-03-18 00:15:02 +0000

    FRB-39 Réaliser le filtrage des transaction par status

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -2,7 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\Auteur;
+use App\Models\Badge;
+use App\Models\Lecteur;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
@@ -16,8 +21,22 @@ class TransactionFactory extends Factory
      */
     public function definition(): array
     {
+        $transactiontableType = $this->faker->randomElement(['App\Models\Lecteur', 'App\Models\Auteur']);
+        if ($transactiontableType == 'App\Models\Lecteur') {
+            $transactiontableId = Lecteur::factory(); 
+        } else {
+            $transactiontableId = Auteur::factory();  
+        }
+        
         return [
-            //
+            'payment_id' => Str::random(20),
+            'status' => $this->faker->randomElement(['success', 'failed', 'pending']),
+            'amount' => $this->faker->randomFloat(2, 1, 1000),
+            'currency' => $this->faker->randomElement(['MAD']),
+            'created_at' => Carbon::now()->subDays(rand(1, 90)),
+            'transactiontable_type' => $transactiontableType,
+            'transactiontable_id' => $transactiontableId,
+            'badge_id' => Badge::factory(),
         ];
     }
 }
