e2fe74994d91539825d271622738f8a7747d3e39
M'hamed Ouhanane <mhmdeouhnane60@gmail.com>
2025-04-04 00:31:01 +0000

    PEAP-46 Crée les fonctionnalité de l'ORM pour la gestion des livres deans le système

--- app/RepositoryInterfaces/LivreRepositoryInterface.php
+++ app/RepositoryInterfaces/LivreRepositoryInterface.php
@@ -4,5 +4,13 @@ namespace App\RepositoryInterfaces;
 
 interface LivreRepositoryInterface
 {
-    
+    public function getAllLivres($paginate);
+    public function findLivre($id);
+    public function filterLivres($filter, $paginate);
+    public function createLivre($createur, $data);
+    public function updateLivre($data, $livre);
+    public function deleteLivre($livre);
+    public function linkTags($livre, $tag_id);
+    public function deleteLinkTags($livre);
+
 }
