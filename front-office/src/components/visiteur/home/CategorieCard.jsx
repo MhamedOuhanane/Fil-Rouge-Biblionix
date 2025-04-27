e7152bd24cf4a01f01af70b77c9eb6491e290788
M'hamed Ouhanane <mhmdeouhnane60@gmail.com>
2025-04-27 01:37:22 +0100

    FRB-100 Téminer la céation de la section des catégories pour la page d'Accueil

--- front-office/src/components/visiteur/home/CategorieCard.jsx
+++ front-office/src/components/visiteur/home/CategorieCard.jsx
@@ -1,15 +1,23 @@
+import { Link } from "react-router-dom";
 
-const CategorieCard = ({ categorie }) => {
+const CategorieCard = ({ categorie, link }) => {
+    const BASE_URL = "http://127.0.0.1:8000/storage";
+    const logo = categorie.logo ? `${BASE_URL}/${categorie.logo}` : null;
 
     return (
         <div
             key={categorie.id}
-            className="bg-[#FCE3C9] rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 flex flex-col items-center py-6"
+            className="bg-[#FCE3C9] gap-2 rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 flex flex-col items-center py-6"
           >
-            <div className="mb-4">{categorie.icon}</div>
-            <h3 className="text-lg mb-2">{categorie.title}</h3>
-            <p className="text-sm mb-4 text-center px-4">{categorie.description}</p>
-            <Link to={`livre/?categorie_id=${categorie.id}`}
+            <div className="mb-4">
+                <img 
+                    src={`${logo}`} 
+                    className="w-10"
+                    alt={`${categorie.title} logo`} 
+                /> 
+            </div>
+            <h3 className="text-lg mb-5">{categorie.title}</h3>
+            <Link to={link}
               className="bg-[#F4A460] text-white px-4 py-2 rounded hover:bg-orange-400"
             >
               Explorer
--- front-office/src/components/visiteur/home/CategorieSection.jsx
+++ front-office/src/components/visiteur/home/CategorieSection.jsx
@@ -1,40 +1,49 @@
-import React from 'react';
-import { BiblionixLogo } from '../../../Icons/Icons';
+import React, { useEffect, useState } from 'react';
+import { BiblionixLogo, SpinnerLoadingIcon } from '../../../Icons/Icons';
 import CategorieCard from './CategorieCard';
-
-// Données simulées pour les catégories avec contenu
-const categories = [
-  {
-    id: 1,
-    title: 'Fiction',
-    icon: <BiblionixLogo className="w-12 h-12" />, // La couleur est gérée par body { color: #A0522D; }
-    description: 'Romans et histoires captivantes pour tous les âges.',
-    link: '/categories/fiction',
-  },
-  {
-    id: 2,
-    title: 'Histoire',
-    icon: <BiblionixLogo className="w-12 h-12" />,
-    description: 'Livres sur les événements historiques et les biographies.',
-    link: '/categories/histoire',
-  },
-  {
-    id: 3,
-    title: 'Science',
-    icon: <BiblionixLogo className="w-12 h-12" />,
-    description: 'Livres sur les sciences : la nature, les animaux, et plus.',
-    link: '/categories/science',
-  },
-];
+import { fetchCategories } from '../../../services/categorieService';
+import Swal from 'sweetalert2';
 
 const CategorieSection = () => {
+  const [categories, setCategories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  
+  useEffect (() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const dataFetch = await fetchCategories();
+        setCategories(dataFetch.categories);
+      } catch (error) {
+        await Swal.fire({
+          icon: "error",
+          title: "Erreur de récupération",
+          text: error.message,
+          confirmButtonText: "Réessayer",
+          confirmButtonColor: "#d33",
+        });
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    
+    fetchData();
+  }, [])
+
   return (
     <section className="container mx-auto px-8 md:px-16 py-10">
       <h2 className="text-2xl mb-6 text-center">Explorez Nos Catégories</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {categories.map((category) => (
-          <CategorieCard categorie={category} />
-        ))}
+        {isLoading ? (
+          <div className="flex items-center space-x-2 mt-3">
+            <SpinnerLoadingIcon />
+            <span className="text-amber-700">Chargement...</span>
+          </div>
+        ) : (categories.map((category, index) => (
+            (index <= 3) && <CategorieCard categorie={category} /> 
+          ))
+        )}
       </div>
     </section>
   );
