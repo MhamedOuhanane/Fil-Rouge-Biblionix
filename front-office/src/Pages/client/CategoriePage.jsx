01c1fc8be7bcfe906d6eddba4a3e4b141f212f3d
M'hamed Ouhanane <mhmdeouhnane60@gmail.com>
2025-04-28 02:09:10 +0100

    FRB-101 Ajouter le recherche des catégories et terminé la page des catégories

--- front-office/src/Pages/client/CategoriePage.jsx
+++ front-office/src/Pages/client/CategoriePage.jsx
@@ -3,17 +3,21 @@ import CategoriePageCard from "../../components/visiteur/CategoriePageCard";
 import { fetchCategories } from "../../services/categorieService";
 import Swal from "sweetalert2";
 import { SpinnerLoadingIcon } from "../../Icons/Icons";
+import SearchInput from "../../components/buttons/SearchInput";
 
 const CategoriesPage = () => {
     const [categories, setCategories] = useState([]);
     const [isLoading, setIsLoading] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [message, setMessage] = useState("");
 
     useEffect(() => {
         const fetchData = async () => {
             setIsLoading(true);
             try {
-            const dataFetch = await fetchCategories();
+            const dataFetch = await fetchCategories("", searchTerm);
             setCategories(dataFetch.categories);
+            setMessage(dataFetch.message)
             } catch (error) {
             await Swal.fire({
                 icon: "error",
@@ -28,24 +32,28 @@ const CategoriesPage = () => {
         };
             
         fetchData();
-    }, []);
+    }, [searchTerm]);
+    
 
     return (
         <div className="bg-[#FDF5E6] min-h-screen">
             <section className="bg-[#F5E6CC] py-12 px-6 text-center mb-6">
-                <h1 className="text-4xl font-bold text-[#8B4513] mb-4">Explorez Nos Catégories</h1>
-                <p className="text-gray-600 text-lg max-w-2xl mx-auto">
+                <h1 className="text-xl md:text-4xl font-bold text-[#8B4513] mb-4">Explorez Nos Catégories</h1>
+                <p className="text-gray-600 text-sm md:text-lg max-w-2xl mx-auto">
                     Découvrez une large gamme de catégories pour tous les goûts et intérêts. Que vous soyez passionné de mangas, de science, ou de littérature, nous avons quelque chose pour vous !
                 </p>
             </section>
+            <div className="max-w-md mx-auto mb-8 px-16">
+                <SearchInput  setSearchItem={setSearchTerm} />
+            </div>
 
             {isLoading ? (
             <div className="flex justify-center items-center space-x-2 mt-3">
                 <SpinnerLoadingIcon />
                 <span className="text-amber-700">Chargement...</span>
             </div>
-            ) : (categories.length > 0 ? (
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            ) : (categories && categories.length > 0 ? (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-8 md:px-16 pb-5">
                     {categories.map((categorie) => (
                         <CategoriePageCard
                             key={categorie.id}
@@ -54,7 +62,7 @@ const CategoriesPage = () => {
                     ))}
                 </div>
             ) : (
-                <p className="text-center text-gray-600">{categories?.message}</p>
+                <p className="text-center text-orange-400">{message}</p>
             ))}
         </div>
     );
